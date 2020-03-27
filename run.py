1e79344fad5823304d009e802c82588ef005bced
robibrutoczki <robibrutoczki@googlemail.com>
2020-03-27 10:35:31 +0000

    Forth update

--- run.py
+++ run.py
@@ -1,15 +1,43 @@
-
-
 import os
-from flask import Flask
+import json
+from flask import Flask, render_template,request
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>Hello</h1><h2>, World</h2>"
 
-print(index())
-#if __n__ == "__main__":
- #    app.run(host=os.environ.get("IP"),port=int(os.environ.get("PORT")),debug=True)   
-print(index())
-print("rob")
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    
+    return render_template("member.html", member=member)
+
+
+@app.route('/contact',methods==["GET","POST"])
+def contact():
+    if requests.method == "POST" :
+        print("Hello ! Is there anybody there ?")
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route('/careers')
+def careers():
+    return render_template("careers.html", page_title="Careers")
